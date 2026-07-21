929146d8398d42df2fc2fdf914c5f5f69134ba5f
jusipat <justin10patrick@gmail.com>
2026-07-21 16:00:37 -0600

    Template refactor

--- src/main/java/net/cflip/divena/Config.java
+++ src/main/java/net/cflip/divena/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.cflip.divena;
 
 import java.util.List;
 import java.util.Set;
--- src/main/java/net/cflip/divena/Divena.java
+++ src/main/java/net/cflip/divena/Divena.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.cflip.divena;
 
 import org.slf4j.Logger;
 
@@ -32,8 +32,8 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod {
+@Mod(Divena.MODID)
+public class Divena {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
     // Directly reference a slf4j logger
@@ -65,7 +65,7 @@ public class ExampleMod {
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer) {
+    public Divena(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
