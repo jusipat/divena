0c8f284b6a95bfea5701849be52966ae58e9cef6
jusipat <justin10patrick@gmail.com>
2026-07-21 16:19:41 -0600

    Fixed modid

--- src/main/java/net/cflip/divena/Divena.java
+++ src/main/java/net/cflip/divena/Divena.java
@@ -35,7 +35,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 @Mod(Divena.MODID)
 public class Divena {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "divena";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
