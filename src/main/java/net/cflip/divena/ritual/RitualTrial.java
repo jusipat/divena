368828ebef2dbc2012b73b948c798472df992916
cflip <cflip@cflip.net>
2026-07-27 18:16:00 -0600

    Add trial description popup

--- src/main/java/net/cflip/divena/ritual/RitualTrial.java
+++ src/main/java/net/cflip/divena/ritual/RitualTrial.java
@@ -3,18 +3,20 @@ package net.cflip.divena.ritual;
 import net.cflip.divena.block.blockentity.CelestialAltarBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundSetSubtitleTextPacket;
+import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket;
 import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.BossEvent;
-import net.minecraft.world.attribute.EnvironmentAttributes;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.zombie.Zombie;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.MoonPhase;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -41,9 +43,17 @@ public class RitualTrial {
         remainingTime = totalTime = DURATION;
     }
 
+    private void showPopup(ServerPlayer player, @NonNull Component title, @Nullable Component subtitle) {
+        // For some reason there isn't a proper interface for showing titles in the Java code
+        // so you just have to rawdog it with packets
+        player.connection.send(new ClientboundSetTitleTextPacket(title));
+        if (subtitle != null) {
+            player.connection.send(new ClientboundSetSubtitleTextPacket(subtitle));
+        }
+    }
+
     public void begin(Level level, ServerPlayer player) {
-        MoonPhase moonPhase = level.environmentAttributes().getValue(EnvironmentAttributes.MOON_PHASE, player.position());
-        player.sendOverlayMessage(Component.literal(moonPhase.getSerializedName()));
+        showPopup(player, Component.literal("Ritual Challenge"), Component.literal("Defeat 10 zombies in 30 seconds"));
 
         for (int i = 0; i < totalEnemies; i++) {
             LivingEntity enemy = new Zombie(level);
@@ -92,7 +102,7 @@ public class RitualTrial {
             return;
         }
 
-        bossEvent.setName(Component.literal(remainingEnemies + "/" + totalEnemies + " remain"));
+        bossEvent.setName(Component.literal(remainingEnemies + "/" + totalEnemies + " zombies left"));
         bossEvent.setProgress((float) remainingTime / (float) totalTime);
     }
 }
